6491658fb3e304695d6ed7ad4773daa60a272ad7
Shuang <shuang@Jeans-MacBook-Pro-3.local>
2024-10-02 15:27:52 +0200

    add message when there are no tasks

--- app/src/main/java/com/example/taskit/ui/HomeScreen.kt
+++ app/src/main/java/com/example/taskit/ui/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.example.taskit.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,9 +11,12 @@ import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.taskit.ui.viewmodel.BucketViewModel
 import com.example.taskit.ui.viewmodel.TaskViewModel
@@ -47,6 +52,17 @@ fun HomeScreen(
                             onLoadBucket(bucket.id)
                         },
                         )
+                }
+            }
+            if(buckets.isEmpty()){
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "You don't have any tasks yet",
+                        textAlign = TextAlign.Center
+                    )
                 }
             }
         }
